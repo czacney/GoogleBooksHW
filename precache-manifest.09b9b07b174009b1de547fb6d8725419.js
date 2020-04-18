92d17af4e9ee9d2940624a1e413fd190e94a0934
Casey Zacney <czacney@gmail.com>
2020-04-18 15:04:27 -0400

    Updates

---
+++ precache-manifest.09b9b07b174009b1de547fb6d8725419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6b4e383b06af97507c1b33888a1c17",
+    "url": "/GoogleBooks/index.html"
+  },
+  {
+    "revision": "0feeb44c7efe071c2def",
+    "url": "/GoogleBooks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "931b349a023a7a16f038",
+    "url": "/GoogleBooks/static/js/2.86c6de02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GoogleBooks/static/js/2.86c6de02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0feeb44c7efe071c2def",
+    "url": "/GoogleBooks/static/js/main.9d4dffe9.chunk.js"
+  },
+  {
+    "revision": "458685de30615bd9cf27",
+    "url": "/GoogleBooks/static/js/runtime-main.d1af3fb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GoogleBooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
